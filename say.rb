6bea3989a6788dac90d9b72cda65fb5b1d5aeb55
a <a@as-Virtual-Machine.local>
2023-10-30 20:05:35 -0700

    Upgrade say to v0.1.1 and use openjdk@17 and pipenv in a shell script

--- say.rb
+++ say.rb
@@ -2,9 +2,8 @@
 
 # This class defines a Homebrew formula for the 'say' command-line tool.
 class Say < Formula
-  url 'https://github.com/8ta4/say/releases/download/v0.1.0/say.tar.gz'
-  sha256 '2e4e41ed118f13b87ae46967bd6abbb2bc4bba110cdb34cc3617e33f4f5803b9'
-  version '0.1.0'
+  url 'https://github.com/8ta4/say/releases/download/v0.1.1/say.tar.gz'
+  sha256 '76af82e7bed3dec70f0383fc4d9f2c23e92abd2ae31f3bf9c521b7911fc84507'
 
   depends_on 'haskell-stack' => :build
   depends_on 'pipenv' => :build
@@ -18,7 +17,22 @@ class Say < Formula
     cd libexec / 'hs' do
       system 'stack', '--local-bin-path', '.', 'install'
     end
-    bin.install_symlink libexec / 'hs/say'
+
+    write_script
+
+    # Link the script to the bin directory
+    bin.install_symlink libexec / 'say.sh' => 'say'
+  end
+
+  def write_script
+    (libexec / 'say.sh').write <<~SHELL_SCRIPT
+      #!/bin/bash
+      source "#{libexec}/.venv/bin/activate"
+      JAVA_HOME="$(brew --prefix openjdk@17)" exec "#{libexec}/hs/say" "$@"
+    SHELL_SCRIPT
+
+    # Make the script executable
+    chmod 0o755, libexec / 'say.sh'
   end
 
   def post_install
