7857b4f0c1c5f0d082cbe4efc52197c114664992
a <a@as-Virtual-Machine.local>
2023-12-24 00:39:57 -0800

    Update say to version 0.3.0 and remove quarantine attribute

--- Casks/say.rb
+++ Casks/say.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 cask 'say' do
-  version '0.2.1'
+  version '0.3.0'
   url "https://github.com/8ta4/say/releases/download/v#{version}/say.dmg"
-  sha256 '25d37502c45998c2d6f6b278df6a4c6eeb82c7cfa0d918336b12111432bbcccb'
+  sha256 'e2df83f37a0e888f50bd381bb1f01d20b7d27f511f201861a99a293f308ee8d4'
   depends_on formula: 'ffmpeg'
   depends_on cask: 'visual-studio-code'
   app 'say.app'
+  postflight do
+    system 'xattr', '-c', "#{appdir}/say.app"
+  end
 end
