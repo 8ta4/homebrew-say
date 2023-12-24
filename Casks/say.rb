f6dbf5d2001e2c89eb2dd423976d47f9fdba626a
a <a@as-Virtual-Machine.local>
2023-12-23 17:09:41 -0800

    Bump up 'say' tool version and checksum

--- Casks/say.rb
+++ Casks/say.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 cask 'say' do
-  version '0.2.1'
+  version '0.3.0'
   url "https://github.com/8ta4/say/releases/download/v#{version}/say.dmg"
-  sha256 '25d37502c45998c2d6f6b278df6a4c6eeb82c7cfa0d918336b12111432bbcccb'
+  sha256 'd8482d84d412337d82a741f7dda8c353e4db6ce407b872badce824147c8561e0'
   depends_on formula: 'ffmpeg'
   depends_on cask: 'visual-studio-code'
   app 'say.app'
